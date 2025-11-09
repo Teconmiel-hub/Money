f97a6d45d758ee8c42a20f15d601c71718aa6f0d
Camila Perez Munoz <camilapmunoz@gmail.com>
2025-11-09 22:38:02 +0000

    Sprint 1: Added frontend pages and navigation

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const path = require('path');
 require('dotenv').config();
 
 const app = express();
@@ -10,12 +11,34 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-// Test route
+// Serve static files from client folder
+app.use(express.static(path.join(__dirname, '../client')));
+
+// Routes
 app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/index.html'));
+});
+
+app.get('/concepts', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/concepts.html'));
+});
+
+app.get('/guidance', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/guidance.html'));
+});
+
+app.get('/simulator', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/simulator.html'));
+});
+
+// API test route
+app.get('/api/test', (req, res) => {
   res.json({ message: 'Money App API is running!' });
 });
 
 // Start server
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
+  console.log(`Visit http://localhost:${PORT} to see your app!`);
 });
+
